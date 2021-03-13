b8558cf4736b9bd1bdcc8f9980635e1ad51283f0
Prince Thind <princethind799@gmail.com>
2021-03-13 19:18:59 +0530

    Complete Rewrite with GUI

--- script.js
+++ script.js
@@ -1,61 +1,136 @@
-let i = 0;
-let arr = ["rock", "scissor", "paper"];
-
-function computerPlay()
+const gameObj =
 {
-    let i = Math.floor(Math.random() * 3);
-    return arr[i];
+    rock: { beat: "scissor", lose: "paper" },
+    paper: { beat: "rock", lose: "scissor" },
+    scissor: { beat: "paper", lose: "rock" }
 }
-function playRound()
+const gameArray = ["rock", "paper", "scissor"];
+const resultValues = ["tie", "win", "lose"];
+
+let moves = 0;
+let score1 = 0;
+let score2 = 0;
+
+function playRound(playerMove, computerMove)
 {
-    let playerSelection = prompt("enter your move");
-    playerSelection = playerSelection.toLowerCase();
-    let computerSelection = computerPlay();
-    let result = null;
-    if (computerSelection === playerSelection)
-        result = "tie";
+    if (playerMove == computerMove)
+    {
+        return 0;
+    }
+    let check = (gameObj[playerMove].beat == computerMove);
+
+    if (check)
+    {
+        return 1;
+    }
+    return 2;
+}
+function reset(e)
+{
+    moves = 0;
+    score1 = 0;
+    score2 = 0;
+
+    const p1Score = document.querySelector('.p1Score');
+    const p2Score = document.querySelector('.p2Score');
+
+    p1Score.textContent = "Player score is: " + 0;
+    p2Score.textContent = "Computer score is: " + 0;
+
+    const moveText = document.querySelector('.move-number');
+    moveText.textContent = "Move Number: " + 0;
+
+    const playerMoveText = document.querySelector('.player-move');
+    const computerMoveText = document.querySelector('.computer-move');
+    playerMoveText.textContent = "Player Move: " + "Awaiting Response";
+    computerMoveText.textContent = "Computer Move: " + "Awaiting Response";
+
+    const resultText = document.querySelector('.result');
+    resultText.textContent = "Result of this Round: " + "Awaiting Response";
+
+    const finalResultText = document.querySelector('.final-result');
+    finalResultText.textContent = "Final Result : " + 10 + " More need to be completed"
+
+}
+function update(e)
+{
+    if (moves >= 10)
+    {
+        moves = 0;
+        score1 = 0;
+        score2 = 0;
+    }
+    let move = e.target.textContent;
+    move = move.toLowerCase();
+    let cMove = gameArray[Math.floor(Math.random() * 3)];
+    let result = playRound(move, cMove);
+    let resultString = "";
+    let finalResultString = "";
+    switch (result)
+    {
+        case 0:
+            moves++;
+            resultString = "It's a Tie!";
+            break;
+        case 1:
+            score1++;
+            moves++;
+            resultString = "Player Won!";
+            break;
+        case 2:
+            score2++;
+            moves++;
+            resultString = "Computer Won!";
+            break;
+    }
+
+    const p1Score = document.querySelector('.p1Score');
+    const p2Score = document.querySelector('.p2Score');
+
+    p1Score.textContent = "Player score is: " + score1;
+    p2Score.textContent = "Computer score is: " + score2;
+
+    const moveText = document.querySelector('.move-number');
+    moveText.textContent = "Move Number: " + moves;
+
+    const playerMoveText = document.querySelector('.player-move');
+    const computerMoveText = document.querySelector('.computer-move');
+    playerMoveText.textContent = "Player Move: " + move;
+    computerMoveText.textContent = "Computer Move: " + cMove;
+
+    const resultText = document.querySelector('.result');
+    resultText.textContent = "Result of this Round: " + resultString;
+
+    const finalResultText = document.querySelector('.final-result');
+    if (moves >= 10)
+    {
+        if (score1 > score2)
+        {
+            finalResultString = "Player won!"
+        }
+        else if (score1 < score2)
+        {
+            finalResultString = "Computer won!"
+        }
+        else
+        {
+            finalResultString = "It's a tie!"
+        }
+        finalResultText.textContent = "Final Result : " + finalResultString;
+    }
     else
     {
-        switch (playerSelection)
-        {
-            case "rock":
-                if (computerSelection === "scissor")
-                    result = "won";
-                else
-                    result = "lost";
-                break;
-
-            case "scissor":
-                if (computerSelection === "paper")
-                    result = "won";
-                else
-                    result = "lost";
-                break;
-
-            case "paper":
-                if (computerSelection === "rock")
-                    result = "won";
-                else
-                    result = "lost";
-                break;
-
-            default:
-               result=null;
-        }
+        finalResultText.textContent = "Final Result : " + (10 - moves) + " More need to be completed"
     }
-    return result;
-}
-function game()
-{
-    while (i++ < 5)
-    {
-        let result=playRound();
-        if(result)
-        console.log("player "+result);
-        else
-        console.log("invalid move");
-    }
-}
 
-game();
+}
+const buttonRock = document.querySelector('.b1');
+const buttonPaper = document.querySelector('.b2');
+const buttonScissor = document.querySelector('.b3');
+const buttonReset = document.querySelector('.reset>button');
+
+buttonRock.addEventListener('click', update);
+buttonPaper.addEventListener('click', update);
+buttonScissor.addEventListener('click', update);
+buttonReset.addEventListener('click', reset);
 
